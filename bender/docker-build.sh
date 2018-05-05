4faeb6d5b3fa92b6b46343b38f56dc16b085a9f1
Rodrigo Martinez da Rocha <rodrigogrohl@gmail.com>
2018-05-05 15:47:27 -0300

    Construcao Imagem Bender

---
+++ bender/docker-build.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+# COLOQUE AQUI SEU USUARIO DOCKERHUB
+dockerhub_user=rodrigogrohl
+image_version=0.3.0
+image_name=bender
+
+docker build --no-cache \
+    -t ${dockerhub_user}/${image_name}:${image_version} \
+    -t ${dockerhub_user}/${image_name}:latest . 

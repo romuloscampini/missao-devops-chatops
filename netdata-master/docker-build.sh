63dda079515a1a9b9b057969b2cdbca9973bce59
Rodrigo Martinez da Rocha <rodrigogrohl@gmail.com>
2018-05-05 20:37:04 -0300

    NetData Cluster

--- netdata-master/docker-build.sh
+++ netdata-master/docker-build.sh
@@ -3,7 +3,7 @@
 # COLOQUE AQUI SEU USUARIO DOCKERHUB
 dockerhub_user=rodrigogrohl
 image_version=1.0.0
-image_name=bender
+image_name=netdata
 
 docker build --no-cache \
     -t ${dockerhub_user}/${image_name}:${image_version} \
